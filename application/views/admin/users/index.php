0901b0064da78d4ed06d7f0988de69dd72a9534c
copy112 <copy112@gmail.com>
2010-01-01 04:31:36 +0100

    Updated manage users interface.

--- application/views/admin/users/index.php
+++ application/views/admin/users/index.php
@@ -19,15 +19,21 @@
 			<th>Username</th>
 			<th>Email</th>
 			<th>Last login</th>
+			<th>Actions</th>
 		</tr>
 	</thead>
 	<tbody>
-		<?php foreach ( $users as $v ): ?>
+		<?php foreach ( $users as $k => $v ): ?>
+		<?php if ( $k % 2 ): ?>
+			<tr class="zebra">
+		<?php else: ?>
 			<tr>
+		<?php endif; ?>
 				<td><?php echo $v->id; ?></td>
 				<td><?php echo $v->username; ?></td>
 				<td><?php echo $v->email; ?></td>
 				<td><?php echo $v->verbose( 'last_login' ); ?></td>
+				<td>Edit Delete</td>
 			</tr>
 		<?php endforeach; ?>
 	</tbody>
