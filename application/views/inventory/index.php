da55c1aadffc4bf5526eb7d62adfaff3c9d9dd79
lewis <lewis@delicata.eu>
2010-01-14 11:51:01 +0000

    Inventory

--- application/views/inventory/index.php
+++ application/views/inventory/index.php
@@ -1,12 +1,27 @@
 <?php
-for($i=1; $i<=count($inventory); $i++){
-	$amount=number_format($inventory[$i]['amount']);
-	$s="";
-	$name=$items[$inventory[$i]['item_id']]['name'];
-	if($name[strlen($name)-1]!="s" && $amount!="1"){ $s="s"; }
-	echo "<a id='item' href='#'>".html::image( "assets/images/items/".$items[$inventory[$i]['item_id']]['class']."/".$items[$inventory[$i]['item_id']]['image'] )."<br />$amount $name$s</a>";
+$p=0;
+$page=str_replace('/','',str_replace('/index','',str_replace('/inventory','',$_SERVER['REQUEST_URI'])));
+if(!$page){$page=1;}
+if(count($inventory)>0){
+foreach ( $inventory as $i ){
+$i->item_id->load();
+$amount=number_format($i->amount);
+$s="";
+$name = $i->item_id->name;
+if($name[strlen($name)-1]!="s" && $amount!="1"){ $s="s"; }
+$item[$p]="<a id='item' href='#'>".html::image( "assets/images/items/".$i->item_id->class."/".$i->item_id->image )."<br />$amount $name$s</a>";
+$p++;
 }
-if(count($inventory)==0){
-	echo 'You have no items in your inventory.';
+$i=$page*20-20;
+while($i<=$page*20-1){
+if($i<=count($inventory)-1){
+echo $item[$i];
 }
+$i++;
+}
+}
+else{
+echo 'You have no items in your inventory.';
+}
+echo "<br clear='all' />$pagination";
 ?>
