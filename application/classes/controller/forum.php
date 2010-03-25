2653f2d9f6c4eb4ad80e0484942f9d6a3d8036bc
Curtis Delicata <curtis@>
2010-03-25 22:46:43 +0000

    Title, post

--- application/classes/controller/forum.php
+++ application/classes/controller/forum.php
@@ -37,6 +37,9 @@ class Controller_Forum extends Controller_Frontend {
 	public function action_category ( $id )
 
 	{
+
+		$this->title = 'Forum - Category '."$id";
+
 		if ( !is_numeric( $id ) )
 			die( 'Invalid thread ID' );
 		
@@ -62,6 +65,8 @@ class Controller_Forum extends Controller_Frontend {
 	public function action_topic( $id )
 	{
 		
+		$this->title = 'Forum - Topic '."$id";
+		
 		if ( !is_numeric( $id ) )
 			die( 'Invalid thread ID' );
 		
@@ -82,4 +87,68 @@ class Controller_Forum extends Controller_Frontend {
 		
 	}
 	
+
+ public function action_post()
+ 
+{
+$this->title = 'Forum - Post';
+ 
+$sprig = Sprig::factory('forum_post');
+ 
+// Check if we have a post request
+$post = Validate::factory($_POST)
+->filter(TRUE, 'trim')
+->filter(TRUE, 'htmlspecialchars', array(ENT_QUOTES))
+->rules('title', $sprig->field('title')->rules)
+->rules('content', $sprig->field('content')->rules)
+->callback('captcha', array($this, 'captcha_valid'));
+ 
+$post = Security::xss_clean($post);
+if ($post->check())
+{
+ 
+ 
+// Assign the validated data to the sprig object
+$sprig->values( $post->as_array());
+$sprig->author = $this->user->id;
+$sprig->created_on = time();
+ 
+ 
+try
+{
+// Create the new post
+$sprig->create();
+ 
+// Redirect the user to the login page
+                                $this->request->redirect( 'forum' );
+}
+ 
+catch (Validate_Exception $e)
+{
+// Get the errors using the Validate::errors() method
+$this->errors = $e->array->errors('register');
+}
+ 
+}
+else
+{
+$this->errors = $post->errors('forum/post');
+}
+                $this->template->content = View::factory('forum/post')
+                        ->set( 'errors', $this->errors )
+                        ->set( 'post', $post->as_array() );
+ 
+ 
+}
+ 
+public function captcha_valid(Validate $array, $field)
+        {
+                if ( ! Captcha::valid($array[$field])) $array->error($field, 'invalid');
+        }
+ 
+ 
+ 
+
+
+
 } // End Forum
