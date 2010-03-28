5669528f6131c37fa30898c28d91acc1ab83cffb
unknown <Copy112@.(none)>
2010-03-29 01:04:05 +0200

    Forgot to add the new view.

---
+++ modules/modulargaming/views/message/sent.php
@@ -0,0 +1,28 @@
+<h2>Messages - Sent</h2>
+
+<div class="left w150"><?php echo $sidebar ?></div>
+
+<div class="right w500">
+
+	<?php echo Message::render() ?>
+	
+	<table>
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>From</th>
+				<th>Date</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($messages as $m): ?>
+			<tr>
+				<td><?php echo html::anchor('message/view/'.$m->id, $m->title) ?></td>
+				<td><?php echo $m->from->username ?></td>
+				<td><?php echo $m->time ?></td>
+			</tr>
+			<?php endforeach ?>
+		</tbody>
+	</table>
+	
+</div>
