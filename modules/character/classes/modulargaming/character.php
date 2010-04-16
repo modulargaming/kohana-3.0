cc9318f353e876b6ca306310f915b17dd6b39ed9
Hinton <copy112@gmail.com>
2010-04-17 00:55:38 +0200

    More character related stuffs, moved. Made the classes extendable.

---
+++ modules/character/classes/modulargaming/character.php
@@ -0,0 +1,77 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * 
+ *
+ * @package    Modular Gaming
+ * @author     Oscar Hinton
+ * @copyright  (c) 2010 Oscar Hinton
+ * @license    http://www.modulargaming.com/license
+ */
+
+class Modulargaming_Character {
+	
+	protected $char;
+	
+	protected $max_alignment = '9999';
+	
+	public function __construct( $char = NULL )
+	{
+		$this->char = $char;
+	}
+	
+	public function get_alignments()
+	{
+		
+		$alignments = Jelly::select( 'alignment' )
+			->execute( );
+		
+		return $alignments;
+		
+	}
+	
+	
+	public function percent_alignment( $ali )
+	{
+		return round( $ali / 100 );
+	}
+	
+	public function alignment( $ali = NULL )
+	{
+		if ( !isset ( $ali ) )
+			$ali = $this->char->alignment;
+			
+		if ( !is_numeric( $ali ) )
+			die( 'Not numeric' );
+		
+		$ali = $this->percent_alignment( $ali );
+		
+		foreach ( $this->get_alignments() as $k => $v )
+		{
+			
+			if ( $ali <= $v->min && $ali >= $v->max )
+				return $v->name;
+				
+		}
+		
+		return $ali;
+		
+	}
+	
+	public function max_hp( $char )
+	{
+		
+	}
+	
+	public function percent_hp( $hp = NULL, $maxhp = NULL ) {
+		
+		if ( !isset ( $hp ) )
+			$hp = $this->char->hp;
+		
+		if ( !isset ( $maxhp ) )
+			$maxhp = $this->char->max_hp;
+		
+		return round( ( $hp / $maxhp ) * 100 );
+		
+	}
+	
+}
---
+++ modules/character/classes/modulargaming/character/event.php
@@ -0,0 +1,64 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Class to hook into events.
+ *
+ * @package    Modular Gaming
+ * @author     Oscar Hinton
+ * @copyright  (c) 2010 Oscar Hinton
+ * @license    http://www.modulargaming.com/license
+ */
+
+class Modulargaming_Character_Event {
+	
+	/**
+	 * Hook into before function.
+	 */
+	public static function before()
+	{
+		$t = Event::$data;
+		
+		// If load_character is set and is TRUE, load the character
+		if (isset($t->load_character) AND $t->load_character) {
+			Character_Event::load_character($t);
+		}
+		
+		// If character is loaded, and character is required, make sure we got a character.
+		if (isset($t->load_character) AND $t->load_character AND isset($t->require_character) AND $t->require_character )
+		{
+			if ( ! $t->character->loaded())
+			{
+				Request::instance()->redirect('character/create');
+			}
+		}
+	}
+	
+	/**
+	 * Loads a character, and assigns it to a variable.
+	 * @param $t
+	 */
+	public static function load_character($t)
+	{
+		if ( ! isset($t->character))
+		{
+			$t->character = Jelly::select('character')
+				->where('user', '=', $t->user->id)
+				->load();
+		}
+	}
+	
+	
+	/**
+	 * Hook into the dasboard display system, and assign the character stats view.
+	 */
+	public static function dashboard()
+	{
+		$t = Event::$data;
+	
+		Character_Event::load_character($t);
+	
+		$t->left = View::factory('character/dashboard/status')
+			->set('character', $t->character)
+			->set('char', new Character($t->character));
+	}
+	
+}
