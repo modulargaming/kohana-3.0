d2eaafec67f5395d766804ad9be94a8ad069ed05
Curtis Delicata <curtis@modulargaming.com>
2010-04-21 21:41:40 +0100

    sidebar

---
+++ modules/character/views/character/sidebar.php
@@ -0,0 +1,16 @@
+        <?php if ( $character->loaded() ): ?>
+
+        <h2><?php echo $character->name; ?></h2>
+        <p style="margin-top: -8px;"> Level: <?php echo $character->level; ?> <?php echo $character->race->name; ?> <?php echo $character->class->name; ?></p>
+
+        <span class="bar hp">
+                <?php echo '<span style="width: ' . $char->percent_hp() . '%"></span>'; ?>
+                <p><?php echo $character->hp . ' / '. $character->max_hp; ?></p>
+        </span>
+
+        <?php else: ?>
+
+        <p>You do not have a character yet, <?php echo html::anchor( 'character', 'create one?' ) ?>.</p>
+
+        <?php endif ?>
+
