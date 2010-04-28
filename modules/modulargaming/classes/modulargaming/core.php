a577fd20ffe899779ff3dccda6835625ba1ef6ad
Curtis Delicata <curtis@modulargaming.com>
2010-04-28 17:42:28 +0100

    Update version in core.php

--- modules/modulargaming/classes/modulargaming/core.php
+++ modules/modulargaming/classes/modulargaming/core.php
@@ -9,7 +9,7 @@
  */
 class Modulargaming_Core {
 	
-	const VERSION = 0.5;
+	const VERSION = 0.55;
 	const VERSION_URL = 'http://development.modulargaming.com/projects/modulargaming/repository/revisions/master/raw/version.txt';
 	const VERSION_CACHE_TIME = 3600;
 	
