897dbc3e535c95e0cd47fd31967bbe8013ef49fb
Curtis Delicata <curtis@modulargaming.com>
2010-10-14 22:32:44 +0100

    Navigation cache fix for different groups

--- modules/modulargaming/classes/navigation.php
+++ modules/modulargaming/classes/navigation.php
@@ -19,18 +19,70 @@ class Navigation {
 	public static function render($group, $view)
 	{
 		
-		$data = Kohana::cache('navigation');
+		
+
+// Logged out navigation
+
+if ($group == 0){
+
+$data = Kohana::cache('navigation_logged_out');
 
 if ( ! $data)
                 {
+
+		
 		$data = Jelly::select('navigation')
 			->where('group', '=', $group)
 			->execute();
 		
 
                         // Cache it.
-                        Kohana::cache('navigation', $data, 3600);
+                        Kohana::cache('navigation_logged_out', $data, 3600);
                 }
+
+}
+
+// Logged in navigation
+
+if ($group == 1){
+
+$data = Kohana::cache('navigation_logged_in');
+
+if ( ! $data)
+                {
+
+		
+		$data = Jelly::select('navigation')
+			->where('group', '=', $group)
+			->execute();
+		
+
+                        // Cache it.
+                        Kohana::cache('navigation_logged_in', $data, 3600);
+                }
+
+}
+
+// Admin navigation
+
+if ($group == 2){
+
+$data = Kohana::cache('navigation_admin');
+
+if ( ! $data)
+                {
+
+		
+		$data = Jelly::select('navigation')
+			->where('group', '=', $group)
+			->execute();
+		
+
+                        // Cache it.
+                        Kohana::cache('navigation_admin', $data, 3600);
+                }
+
+}
 
 
 		
