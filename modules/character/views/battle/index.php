1ac71582738d672fe11c9df3856d865a3f71c4f9
Wade Penistone <truemedia@mediacityonline.net>
2011-07-04 00:23:38 +0100

    added attack links with custom get variable and id generated on the fly

--- modules/character/views/battle/index.php
+++ modules/character/views/battle/index.php
@@ -59,6 +59,7 @@ echo html::image($image);
 			'9' => "Delayed detonations"
 		);
 
+	$chr_atk_id = 0;
 	foreach($atk_names as $atk_name){
 		$r = rand(0, 255);
 		$g = rand(0, 255);
@@ -66,7 +67,8 @@ echo html::image($image);
 		$ri = 255 - $r;
 		$gi = 255 - $g;
 		$bi = 255 - $b;
-		echo html::anchor( 'battle/attack', ' '.$atk_name.' ', array('style' => "color: rgb(".$r.",".$g.",".$b."); background-color: rgb(".$ri.",".$gi.",".$bi."); border: 1px solid black; padding: 5px;")); 
+		echo html::anchor( 'battle/attack?chr_atk='.$chr_atk_id, ' '.$atk_name.' ', array('style' => "color: rgb(".$r.",".$g.",".$b."); background-color: rgb(".$ri.",".$gi.",".$bi."); border: 1px solid black; padding: 5px;")); 
+		$chr_atk_id++;
 	} ?><br />
 	<?php echo html::anchor( 'battle/run', 'Try to run away' ); ?><br />
 	
