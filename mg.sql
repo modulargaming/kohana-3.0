b7738a13378356c4ab490ca1494cb9bb20c108b6
Wade Penistone <truemedia@mediacityonline.net>
2011-07-04 00:15:51 +0100

    set battle table atk columns to NULL to allow blank values on startup to avoid errors

--- mg.sql
+++ mg.sql
@@ -128,8 +128,8 @@ CREATE TABLE IF NOT EXISTS `battles` (
   `character_id` int(6) NOT NULL,
   `monster_id` int(6) NOT NULL,
   `hp` int(6) NOT NULL,
-  `character_atk` int(6) NOT NULL,
-  `monster_atk` int(6) NOT NULL,
+  `character_atk` int(6) NULL,
+  `monster_atk` int(6) NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
